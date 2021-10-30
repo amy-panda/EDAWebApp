3de6fc96bdcc0a37b8d91a96d0c439c0f73be358
Tina Li <yanting.li@student.uts.edu.au>
2021-10-31 02:47:33 +1100

    update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,1 +1,45 @@
 # To be filled by students
+
+import streamlit as st
+import pandas as pd 
+import src.data as data 
+import src.datetime as datetime
+import src.numeric as numeric
+import src.text as text
+
+def main():
+    st.title("Data Explorer Tool")
+
+    menu = ["Dataset","About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Dataset":
+        data_file = st.file_uploader("Choose a CSV file",type = ["csv"])
+        if st.button("Process"):
+            if data_file is not None:
+                df = pd.read_csv(data_file)
+                dataTypeSeries = df.dtypes
+
+                st.write("""**Types of Columns**""")
+                st.write(dataTypeSeries.astype(str))
+
+                st.write("""**Top Rows of Table**""")
+                st.dataframe(df.head(5))
+            
+            # change the datetime column type
+                datetime.transform(df)
+                st.dataframe()
+
+            # Display name of column as subtitle
+                datecol = "column name of date values"
+                st.subheader('4.0 Field Name: {}'.format(datecol))           
+
+    else:
+        st.subheader("About")
+        st.text("DSP - AT3 Streamlit Web App")
+        st.text("Group 9")
+		
+
+
+if __name__ == '__main__':
+	    main()
