f7c11d23295a18f8aea5e066a49bb968adb7435f
Amy Yang <yan.yang@student.uts.edu.au>
2021-11-01 21:33:13 +1100

    upload text.py script

--- src/text.py
+++ src/text.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from dataclasses import dataclass
 import pandas as pd
-
+import altair as alt
 
 @dataclass
 class TextColumn:
@@ -13,71 +13,89 @@ class TextColumn:
     """
     Return name of selected column
     """
-    return None
+    # self.col_name=pd.DataFrame(self).columns
+    return self.col_name
 
   def get_unique(self):
     """
     Return number of unique values for selected column
     """
-    return None
+    # return self.nunique()
+    return self.serie.unique().shape[0]
 
   def get_missing(self):
     """
     Return number of missing values for selected column
     """
-    return None
+    # return self.isna().sum()
+    return self.serie.isnull().sum()
+    
 
   def get_empty(self):
     """
     Return number of rows with empty string for selected column
     """
-    return None
+    # return (self=='').sum()
+    return self.serie[self.serie==''].count()
 
   def get_whitespace(self):
     """
     Return number of rows with only whitespaces for selected column
     """
-    return None
+    return self.serie.str.isspace().sum()
 
   def get_lowercase(self):
     """
     Return number of rows with only lower case characters for selected column
     """
-    return None
+    return self.serie.str.islower().sum()
 
   def get_uppercase(self):
     """
     Return number of rows with only upper case characters for selected column
     """
-    return None
+    return self.serie.str.isupper().sum()
   
   def get_alphabet(self):
     """
     Return number of rows with only alphabet characters for selected column
     """
-    return None
+    return self.serie.str.isalpha().sum()
 
   def get_digit(self):
     """
     Return number of rows with only numbers as characters for selected column
     """
-    return None
+    return self.serie.str.isdigit().sum()
 
   def get_mode(self):
     """
     Return the mode value for selected column
     """
-    return None
+    return self.serie.mode()
 
 
   def get_barchart(self):
     """
     Return the generated bar chart for selected column
     """
-    return None
+
+    df2=pd.DataFrame({
+      self.col_name:self.serie.unique(),
+      'Count of Records':self.serie.value_counts()
+    })
+
+    return alt.Chart(df2).mark_bar().encode(
+        x=alt.X(self.col_name,sort=None),
+        y='Count of Records')
+
 
   def get_frequent(self):
     """
     Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
     """
-    return None
+    sr=self.serie
+    df_f=pd.DataFrame({'occurrence':sr.value_counts(), 'percentage':sr.value_counts(normalize=True)}).reset_index().rename(columns={'index':'value'})
+    return df_f
+
+  
