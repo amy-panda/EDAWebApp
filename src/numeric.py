91ae5443bc54f08b113c7ff2a9a83ba1f35201c6
maedehme <shakiba_mehdizadeh@yahoo.com>
2021-11-07 00:33:41 +1100

    Update numeric.py

--- src/numeric.py
+++ src/numeric.py
@@ -3,12 +3,12 @@ import streamlit as st
 import seaborn as sns
 from dataclasses import dataclass
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 
 @dataclass
 class NumericColumn:
-
+  
   col_name: str
   serie: pd.Series
  
@@ -43,7 +43,7 @@ class NumericColumn:
     return self.serie.median()
 
   def get_histogram(self):
-    return sns.histplot(self.serie)
+    return plt.hist(self.serie, bins = 50)
 
   def get_frequent(self):
 
